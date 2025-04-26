e3e5182d0279870febd9a6d096305a0727eae6f2
anantd <anantd@uw.edu>
2025-04-26 14:29:01 -0700

    Initial Commit

---
+++ iKid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iKid
+//
+//  Created by Anant Dhokia on 4/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iKidTests/iKidTests.swift
@@ -0,0 +1,17 @@
+//
+//  iKidTests.swift
+//  iKidTests
+//
+//  Created by Anant Dhokia on 4/26/25.
+//
+
+import Testing
+@testable import iKid
+
+struct iKidTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
